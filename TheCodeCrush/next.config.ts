cd357bb4706e6f6b7aaa7eade3e3cf844010b8ea
RISHI  KUMAR  THAKUR <119896306+RISHIKUMARTHAKUR@users.noreply.github.com>
2025-11-02 16:50:02 +0530

    bugfix

--- TheCodeCrush/next.config.ts
+++ TheCodeCrush/next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
